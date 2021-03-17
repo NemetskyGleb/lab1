51bf025d7bcb752db614b7fa1bd748c31ad8a922
Gleb Nemetskiy <glebdvp@gmail.com>
2021-03-17 14:28:13 +0600

    Добавлен абстрактный класс Company
    Добавлены член данные класса и две чистые виртуальные функции

--- company.h
+++ company.h
@@ -1,4 +1,22 @@
 #ifndef COMPANY_H
 #define COMPANY_H
+#include <QString>
+#include <QVector>
+
+class Company
+{
+private:
+    QString name_;
+    QVector<QString> owners_;
+    double income_;
+    double area_;
+    int numberOfEmployees_;
+    enum type { Private, Public, Multinational, Government };
+public:
+    virtual type getCompanyType() = 0;
+    virtual double getTaxPerMonth() = 0;
+};
+
+
 
 #endif // COMPANY_H
